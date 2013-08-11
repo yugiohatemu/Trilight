93b18f2a221da8638a9060d6bacbd0784250f70f
Yue <yue4huang@gmail.com>
2013-08-11 09:56:28 -0400

    Init new project

---
+++ Trilight/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Trilight
+//
+//  Created by Yue on 8/11/13.
+//  Copyright (c) 2013 Yue. All rights reserved.
+//
+
+#include <iostream>
+#include "SDL/SDL.h"
+
+int main( int argc, char* args[] ){
+    //Start SDL
+    SDL_Init( SDL_INIT_EVERYTHING );
+    
+    //Quit SDL
+    SDL_Quit();
+    
+    return 0;
+}
